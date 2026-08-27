731d68fb18173470d379e74f538a7ccef6f814ac
Jakub Zbrzeżny <jakub.zbrzezny@abdn.ac.uk>
2026-08-27 22:48:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-fetch("includes/footer.html")
+fetch("/includes/nav.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("nav").innerHTML = data;
+    });
+
+fetch("/includes/footer.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("footer").innerHTML = data;
