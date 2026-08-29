7f6cd0981f9c37c759815b4db1b84e6e8156862f
Jakub Zbrzeżny <jakub.zbrzezny@abdn.ac.uk>
2026-08-29 14:51:45 +0100

    Update script.js

--- read/script.js
+++ read/script.js
@@ -1,35 +1,35 @@
 const dialogues = { 
     
     gym:    [
-["مرحبا كوتش","Hello, coach!"],
-["مرحبا","Hello."],
-["كيفك","How are you?"],
-["الحمد لله","Thanks God. "],
-["انت كيفك","How about you?"],
-["تمام الحمد لله","I'm okay, thanks God. "],
-["جاهز للتدريب","Are you ready for the training? "],
-["اكيد جاهز ومتحمس","Sure! Ready and motivated! "],
-["تمام تفضل معي","Great. Please follow me. "],
-["تمام","Okay. "],
-["رح نبدا بحماس قوي وشو بحكيلك اشتغل","We'll start at full power. Train as I tell you!"],
-["تمام كوتش","Sure, coach. "],
-["اركض عشر دقائق وبعد ما تخلص احكيلي","Run ten minutes, and tell me when you finish. "],
-["تمام كوتش","Sure, coach. "],
-["خلصت كوتش","Coach, I've finished!"],
-["تمام تفضل معي عل ماكنه الصدر","Great. Please follow me to the chest-machine. "],
-["تمام كيف ادرب كوتش","Sure. How do I train [here]? "],
-["بتتمدد عل ظهرك وبتبلش ترفع شوي شوي قد ما تقدر","You should lie down on your back and start pushing up.[Just] slowly [and] as much as you can. "],
-["تمام كوتش","Sure, coach. "],
-["كوتش خلصت","Coach, I've finished!"],
-["تمام يلا اللي بعدو","Great. Let's go for the next one. "],
-["تمام كوتش خلصت","Okay, coach, I've finished!"],
-["يعطيك العافيه","May He give you strength. "],
-["الله يعافيك","May God strengthen you. "],
-["تمام كوتش بكرا اي ساعه","Okay, coach. What time tomorrow? "],
-["بكرا عل اربعه تمام لالك","Does it work for you at four tomorrow?"],
-["تمام منيح","Yes, it's okay. "],
-["سلام","Bye! "],
-["سلام بكرا بشوفك","Bye, I'll see you tomorrow. "]
+["مرحبا كوتش","Hello, coach!","مرحبا كوتش!"],
+["مرحبا","Hello.","مرحبا!"],
+["كيفك","How are you?","كيف حالك؟"],
+["الحمد لله","Thanks God. ","نحمد الله."],
+["انت كيفك","How about you?","كيف حالك أنت؟"],
+["تمام الحمد لله","I'm okay, thanks God. ","الحمد لله بكل خير."],
+["جاهز للتدريب","Are you ready for the training? ","هل أنت جاهز للتدريب اليوم؟"],
+["اكيد جاهز ومتحمس","Sure! Ready and motivated! ","بكل تأكيد أنا جاهز كل الجهوزية ومتحمس جدا."],
+["تمام تفضل معي","Great. Please follow me. ","تمام هيا تفضل معي."],
+["تمام","Okay. ","تمام."],
+["رح نبدا بحماس قوي وشو بحكيلك اشتغل","We'll start at full power. Train as I tell you!","سوف نبدأ بحماس قوي ومثلما أحكي لك اعمل."],
+["تمام كوتش","Sure, coach. ","تمام كوتش."],
+["اركض عشر دقائق وبعد ما تخلص احكيلي","Run ten minutes, and tell me when you finish. ","اركض ركض عشر دقائق وعندما تنتهي اخبرني."],
+["تمام كوتش","Sure, coach. ","تمام كوتش."],
+["خلصت كوتش","Coach, I've finished!","كوتش لقد أكملت."],
+["تمام تفضل معي عل ماكنه الصدر","Great. Please follow me to the chest-machine. ","من فضلك تفضل معي على ماكينة الصدر."],
+["تمام كيف ادرب كوتش","Sure. How do I train [here]? ","تمام."],
+["بتتمدد عل ظهرك وبتبلش ترفع شوي شوي قد ما تقدر","You should lie down on your back and start pushing up.[Just] slowly [and] as much as you can. ","تمدد على ظهرك وابدأ الرفع ببالتدريج على قدر ما تستطيع."],
+["تمام كوتش","Sure, coach. ","تمام كوتش."],
+["كوتش خلصت","Coach, I've finished!","كوتش لقد أكملت."],
+["تمام يلا اللي بعدو","Great. Let's go for the next one. ","جيد من فضلك أعمل التمرين الذي يلية."],
+["تمام كوتش خلصت","Okay, coach, I've finished!","لقد أكملت كوتش."],
+["يعطيك العافيه","May He give you strength. ","أعطاك الله العافية."],
+["الله يعافيك","May God strengthen you. ","وأنت أيضا اعطاك الله العافية."],
+["تمام كوتش بكرا اي ساعه","Okay, coach. What time tomorrow? ","تمام على أي ساعة التمرين غدا كوتش."],
+["بكرا عل اربعه تمام لالك","Does it work for youat four tomorrow?","غدا على الساعة الرابعة جيد لك؟"],
+["تمام منيح","Yes, it's okay. ","تمام جيد جدا."],
+["سلام","Bye! "," سلام!"],
+["سلام بكرا بشوفك","Bye, I'll see you tomorrow. ","سلام غدا أراك!"]
   ],
 
     gym2: [
@@ -52,6 +52,7 @@ function renderDialogue(dialogueId) {
             <div class="dialogue-line">
                 <div class="arabic" dir="rtl">${ar}</div>
                 <div class="english">${en}</div>
+                <div class="arabicmsa" dir="rtl">${msa}</div>
             </div>
         `;
 
@@ -91,6 +92,16 @@ function showSideBySide(dialogueId) {
 
         line.classList.remove("below");
         line.classList.add("side-by-side");
+
+    });
+}
+
+function showMSA(dialogueId) {
+
+    document.querySelectorAll(
+        "#dialogue-" + dialogueId + " .arabicMSA"
+    ).forEach(line => {
+        line.classList.remove("below");
 
     });
 }
