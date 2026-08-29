e326cd216731bae12b88e2f2f1f44f8049454c86
Jakub Zbrzeżny <jakub.zbrzezny@abdn.ac.uk>
2026-08-29 02:14:53 +0100

    Create script.js

---
+++ read/script.js
@@ -0,0 +1,36 @@
+function showArabicOnly() {
+    document.querySelectorAll('.dialogue-line').forEach(line => {
+        line.classList.remove('below', 'side-by-side');
+    });
+}
+
+function showBelow() {
+    document.querySelectorAll('.dialogue-line').forEach(line => {
+        line.classList.remove('side-by-side');
+        line.classList.add('below');
+    });
+}
+
+function showSideBySide() {
+    document.querySelectorAll('.dialogue-line').forEach(line => {
+        line.classList.remove('below');
+        line.classList.add('side-by-side');
+    });
+}
+
+const dialogue = [
+    ["AR", "ENG"],
+    ["AR", "ENG""],
+    ["AR", "ENG""]
+];
+
+const container = document.getElementById("dialogue");
+
+dialogue.forEach(([ar, en]) => {
+    container.innerHTML += `
+        <div class="dialogue-line">
+            <div class="arabic">${ar}</div>
+            <div class="english">${en}</div>
+        </div>
+    `;
+});
